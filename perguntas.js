d9d4ba69f5a656eb51667ebdb674427722fba75d
Bruhsena <00001105684404sp@al.educacao.sp.gov.br>
2024-11-27 10:30:15 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Filme',
+    'Quel o nome do personagem que é azul e tem orelhas grandes?',
+    'O nome é Stitch'
+)
+
+criaCartao(
+    'Filme',
+    'Qual o nome da personagem que vive trancada em uma torre?',
+    'O nome é Rapunzel'
+)
+
+criaCartao(
+    'Série',
+    'Qual o nome da série em que as personagens tem q achar A ?',
+    'O nome da série é Pretty Litle Liars'
+)
+
+criaCartao(
+    'Série',
+    'Qual a frase que Dereck Shepherd sempre fala antes de iniciar uma cirurgia ?',
+    'Hoje é um lindo dia para salvar vidas'
+)
